e16cc5b26e4cfa6b313194a8a1307624c1a913d2
Aluno TripleTen <wesleyalmada@yahoo.com.br>
2024-07-13 08:22:12 -0300

    updated Navigation.js and created navigation.css, added images for menu Nav when in small screen, Navigation.js imported to App.js and navigation.css to index.css

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Header from './Header';
+import Navigation from './Navigation';
 import Main from './Main';
 import Random from './Random'
 import About from './About'
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="page">
       <Header />
+      <Navigation />
       <Main>
       <Routes>
         <Route path="/" element={<Random />} />
