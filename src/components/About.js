337f6439f71703b29f5e6d987fdfa532264dbac6
Aluno TripleTen <wesleyalmada@yahoo.com.br>
2024-07-13 07:47:40 -0300

    About.js modified and imported to App.js as a route, about.css created and imported in index.css

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <div className="about-me">
+      <h2 className="about-me__title">About Me</h2>
+      <p className="about-me__text">
+        Hello, my name is Wesley, a technology and software development enthusiast. I am constantly seeking to learn new skills and improve my knowledge in the field of programming.
+      </p>
+      <p className="about-me__text">
+        This website was created as part of my project portfolio, where I share my work and experiences.
+      </p>
+      <p className="about-me__text">
+        Besides website development, I am also interested in game development. Outside the world of technology, I enjoy reading and playing games.
+      </p>
+    </div>
+  );
+};
+
+export default AboutMe;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Header from './Header';
 import Main from './Main';
+import About from './About'
 import Footer from './Footer';
 
 function App() {
   return (
     <div className="page">
         <Header />
-        <Main />
+        <Main>
+        <Routes>
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Main>
         <Footer />
     </div>
   );
