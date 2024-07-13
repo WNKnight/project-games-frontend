2ccdbdb35a1f988892a428e44dfd259b1a4e7b72
Aluno TripleTen <wesleyalmada@yahoo.com.br>
2024-07-13 07:13:39 -0300

    main.css created and added in index.css , main.js modified and imported in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Header from './Header'
+import Header from './Header';
+import Main from './Main';
 
 function App() {
   return (
     <div className="page">
         <Header />
+        <Main />
     </div>
   );
 }
