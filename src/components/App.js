dd4c2d5e2a48e67d6f1dbff2ccc8f2e2b4639cda
Aluno TripleTen <wesleyalmada@yahoo.com.br>
2024-07-15 09:07:19 -0300

    created NotFound.js for non existent pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Header from './Header';
 import Navigation from './Navigation';
 import Main from './Main';
-import Random from './Random'
-import Catalog from './Catalog'
+import Random from './Random';
+import Catalog from './Catalog';
 import SearchResults from './SearchResults';
 import GameDetails from './GameDetails';
-import FranchiseDetails from './FranchiseDetails'
-import CharacterDetails from './CharacterDetails'
-import About from './About'
+import FranchiseDetails from './FranchiseDetails';
+import CharacterDetails from './CharacterDetails';
+import About from './About';
+import NotFound from './NotFound';
 import Footer from './Footer';
 
 function App() {
@@ -18,16 +19,18 @@ function App() {
       <Header />
       <Navigation />
       <Main>
-      <Routes>
-        <Route path="/" element={<Random />} />
-        <Route path="/search-results" element={<SearchResults />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/catalog/:page" element={<Catalog />} />       
-        <Route path="/game/:id" element={<GameDetails />} />
-        <Route path="/franchise/:id" element={<FranchiseDetails />} />
-        <Route path="/character/:id" element={<CharacterDetails />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Random />} />
+          <Route path="/search-results" element={<SearchResults />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/catalog/:page" element={<Catalog />} />
+          <Route path="/game/:id" element={<GameDetails />} />
+          <Route path="/franchise/:id" element={<FranchiseDetails />} />
+          <Route path="/character/:id" element={<CharacterDetails />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/404" />} />
+        </Routes>
       </Main>
       <Footer />
     </div>
