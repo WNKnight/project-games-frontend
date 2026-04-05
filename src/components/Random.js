a23e87425e4b992bef5be0ecb1d26d7175930ce4
Wesley Monteiro <wesleyalmada@yahoo.com.br>
2026-04-05 16:18:18 -0300

    fix: correct random refresh loading behavior to use skeleton instead of spinner

--- src/components/Random.js
+++ src/components/Random.js
@@ -29,7 +29,10 @@ function Random() {
 
   const handleRefresh = () => {
     setLoading(true);
-    loadRandomGames();
+    setTimeout(() => {
+      setGames([]);
+      loadRandomGames();
+    }, 150);
   };
 
   return (
