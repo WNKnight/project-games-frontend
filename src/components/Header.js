562e07b172a07ea4259bf745e50b4c7851834826
Wesley Monteiro <wesleyalmada@yahoo.com.br>
2026-04-05 15:38:05 -0300

    feat: redesign header and navigation with responsive mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navigation from './Navigation';
 import SearchForm from './SearchForm';
 import headerLogo from '../images/header-logo.png';
-
+import navIcon from '../images/menu-icon.svg';
 
 function Header() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobile(!isMobile);
+  };
+
   return (
     <header className="header">
       <div className="header__left">
+        <span className="navigation__open-btn" onClick={toggleMenu}>
+          <img src={navIcon} className="navigation__icon" alt="Menu" />
+        </span>
+
         <img src={headerLogo} alt="Game Explorer Logo" className="header__logo" />
         <h1 className="header__title">Game Explorer</h1>
       </div>
-      <Navigation />
+
+      <Navigation isMobile={isMobile} toggleMenu={toggleMenu} />
+
       <div className="header__right">
         <SearchForm />
       </div>
     </header>
   );
-};
+}
 
 export default Header;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,30 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import navIcon from '../images/menu-icon.svg';
 import closeBtn from '../images/cross-close.svg';
 
-function Navigation() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMobile(!isMobile);
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth > 768) {
-        setIsMobile(false);
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-    handleResize();
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
+function Navigation({ isMobile, toggleMenu }) {
   return (
     <>
       <nav className={`navigation ${isMobile ? 'navigation--hidden' : ''}`}>
@@ -40,12 +18,10 @@ function Navigation() {
           </li>
         </ul>
       </nav>
-
       <div className={`navigation__sidenav ${isMobile ? 'navigation__sidenav--active' : ''}`}>
         <span className="navigation__close-container" onClick={toggleMenu}>
           <img className="navigation__close-btn" src={closeBtn} alt="Close Menu" />
         </span>
-
         <ul className="navigation__sidenav-list">
           <li className="navigation__sidenav-item">
             <Link to="/" className="navigation__sidenav-link" onClick={toggleMenu}>Home</Link>
@@ -58,15 +34,8 @@ function Navigation() {
           </li>
         </ul>
       </div>
-
-      <span
-        className={`navigation__open-btn ${isMobile ? 'navigation__open-btn--hidden' : ''}`}
-        onClick={toggleMenu}
-      >
-        <img className="navigation__icon" src={navIcon} alt="Open Menu" />
-      </span>
     </>
   );
-};
+}
 
 export default Navigation;
