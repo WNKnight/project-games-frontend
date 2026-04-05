fd4809dc86d0b6fecb034452e4ff4f1d16470d25
Wesley Monteiro <wesleyalmada@yahoo.com.br>
2026-04-05 02:58:31 -0300

    feat: redesign header with integrated navigation and search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Navigation from './Navigation';
 import SearchForm from './SearchForm';
+import headerLogo from '../images/header-logo.png';
 
 
 function Header() {
   return (
     <header className="header">
-      <h1 className="header__title">Game Explorer</h1>
+      <div className="header__left">
+        <img src={headerLogo} alt="Game Explorer Logo" className="header__logo" />
+        <h1 className="header__title">Game Explorer</h1>
+      </div>
       <Navigation />
-      <SearchForm />
+      <div className="header__right">
+        <SearchForm />
+      </div>
     </header>
   );
 };
